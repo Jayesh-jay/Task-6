b5146c38bac194ddbd3831618e1d3cd0712d2516
Jayesh-jay <151343741+Jayesh-jay@users.noreply.github.com>
2023-11-22 21:20:18 +0530

    Add files via upload

---
+++ additon_first & last _num.py
@@ -0,0 +1,7 @@
+number = input("Enter the number:")
+number = str(number)
+first_digit = int(number[0])
+last_digit = int(number[-1])
+sum= first_digit + last_digit
+print('Sum of first and last digit of the number is',sum) 
+      
